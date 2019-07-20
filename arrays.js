bcbd9950020524b877dbc59b275802ae33d8695b
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-20 16:37:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
-function accessElementInArray(array, element) {
+function accessElementInArray(array,) {
   return [element, ...array] 
 }
