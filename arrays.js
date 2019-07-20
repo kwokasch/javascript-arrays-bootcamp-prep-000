df460b087bc75ac8274d70961a3180c7242b7c06
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-20 16:29:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array] 
 }
 
-function addElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
   return [element, ...array] 
 }
